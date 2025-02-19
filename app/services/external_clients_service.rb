cc8a0ebde11ab3aef376875ea036b3471ede735c
Miqueias Silva <miqueias.ufpi@gmail.com>
2025-02-18 22:59:47 -0300

    Update external_clients_service service

--- app/services/external_clients_service.rb
+++ app/services/external_clients_service.rb
@@ -5,17 +5,15 @@ require "cpf_cnpj"
 class ExternalClientsService
   API_URL = "https://jsonplaceholder.typicode.com/users"
 
-  def self.fetch_clients
+  def self.fetch_and_store_clients
     response = Net::HTTP.get(URI(API_URL))
     clients = JSON.parse(response)
 
-    clients.map do |client|
-      {
-        id: client["id"],
-        name: client["name"],
-        email: client["email"],
-        cpf: CPF.generate(true) # Gera um CPF formatado
-      }
+    clients.each do |client_data|
+      Client.find_or_create_by(email: client_data["email"]) do |client|
+        client.name = client_data["name"]
+        client.cpf = CPF.generate(true) # Gera um CPF formatado
+      end
     end
   end
 end
